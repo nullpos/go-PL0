13337f63e41c5643479d0e466b0a19deb3b6e8d9
null44 <greenstar77@gmail.com>
2015-06-12 11:59:21 +0900

    add some codes

--- main.go
+++ main.go
@@ -1,76 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"sync"
 
-	"PL0/types"
+	"go-PL0/types"
 )
+
+var b []byte
 
 // GetToken output token to ch.
 func GetToken(f *os.File, ch chan *types.Token) {
-	b := make([]byte, types.MaxName)
-	o := 0
-
+	getNextLine := nextLiner(f)
 	for {
-		n, _ := f.ReadAt(b, int64(o))
-		if n == 0 {
-			break
-		}
+		if getNextLine() {
 
-		for i := 0; i < n; i++ {
-			var token = new(types.Token)
-			if b[i] == ' ' || b[i] == '\t' {
-				o++
-				continue
-			}
-			cc := types.CharClassType[b[i]]
-			switch cc {
-			case types.Letter:
-				o++
-				break
-			case types.Digit:
-				o++
-				break
-			case types.Colon:
-				if b[i+1] == '=' {
-					token.Kind = types.Assign // :=
-					o += 2
-				} else {
-					token.Kind = types.Nul
-					o++
-				}
-				ch <- token
-				break
-			case types.Lss:
-				if b[i+1] == '=' {
-					token.Kind = types.LssEq // <=
-					o += 2
-				} else if b[i+1] == '>' {
-					token.Kind = types.NotEq // <>
-					o += 2
-				} else {
-					token.Kind = types.Gtr // <
-					o++
-				}
-				ch <- token
-				break
-			case types.Gtr:
-				if b[i+1] == '=' {
-					token.Kind = types.GtrEq // >=
-					o += 2
-				} else {
-					token.Kind = types.Gtr // >
-					o++
-				}
-				ch <- token
-				break
-			default:
-				token.Kind = cc
-				o++
-				break
-			}
+		} else {
+			break
 		}
 	}
 	close(ch)
@@ -78,7 +26,7 @@ func GetToken(f *os.File, ch chan *types.Token) {
 
 func main() {
 	types.InitCharClassType()
-	filename := "../src/PL0/test.pl1"
+	filename := "../go-PL0/test.pl1"
 	file, e := os.Open(filename)
 	if e != nil {
 		fmt.Println(e)
@@ -102,4 +50,15 @@ func main() {
 
 	wg.Wait()
 	fmt.Println("Done")
+}
+
+func nextLiner(f *os.File) func() bool {
+	scanner := bufio.NewScanner(f)
+	return func() bool {
+		if scanner.Scan() {
+			b = scanner.Bytes()
+			return true
+		}
+		return false
+	}
 }
